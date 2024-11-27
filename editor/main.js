57e2fa70de3eca222a80ac97b4a7ade7c0dfd7ba
Your Name <you@example.com>
2024-11-27 15:03:51 +0500

    add .

--- editor/main.js
+++ editor/main.js
@@ -4,5 +4,42 @@ function run (){
     let js = document.getElementById("js").value;
     let output = document.getElementById("output");
     output.contentDocument.body.innerHTML= html + "<style>" + css +"</style>";
-    output.contentWindow.eval(js)
+    output.contentWindow.eval(js);
 }   
+function logout () {
+    let data  = localStorage.getItem ("");
+let save = data ? JSON.parse(data) :[];
+let found = save.some(save => save.email);
+if (found){
+    Swal.fire({
+        title: "Do you want to Logout?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        denyButtonText: `No`
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("Saved!", "", "success");
+    localStorage.removeItem("save");
+    setTimeout(() =>{
+window.location.href = "../Login& signup/login index.html"
+    }, 2000)
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+          
+        }
+    });
+}else {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Data not found, please sign in?</a>'
+      }).then(() => {
+        setTimeout(() => {
+          window.location.href = "../Login& signup/signup.html";
+        }, 500);
+      });
+}
+
+}
